0b076b517528493b15cc9d2b22521aa80cd1893e
ComedyStudios <39218621+MischaWillKekse@users.noreply.github.com>
2020-05-31 16:07:26 +0200

    trying to save the branch
    trying to save the branch

---
+++ cube 1.6/cubeWPF/Cordinates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cubeWPF
+{
+    public class Cordinate
+    {
+        public double x;
+        public double y;
+        public double z;
+    }
+    
+}
---
+++ cube 1.6/cubeWPF/Lines.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cubeWPF
+{
+    class Lines
+    {
+        public int id1;
+        public int id2;
+    }
+}
